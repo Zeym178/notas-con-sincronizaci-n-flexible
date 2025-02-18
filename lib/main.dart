aca8f78dcc11ff8724bb3cd0b93611ffb746e916
Zeym <zeemancoro@gmail.com>
2025-02-18 13:27:45 -0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/adapters.dart';
@@ -6,7 +7,20 @@ import 'package:notesapp/auth/main_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: 'AIzaSyAqxAwMVX9ZxLbovthwVkl3sYDIn1rRRe0',
+        authDomain: 'zeym-s-notes-app.firebaseapp.com',
+        appId: '1:455650982872:web:cf646af459194c9d8190ad',
+        messagingSenderId: '455650982872',
+        projectId: 'zeym-s-notes-app',
+        storageBucket: 'zeym-s-notes-app.firebasestorage.app',
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
   await Hive.initFlutter();
   await Hive.openBox('notes');
   runApp(const MyApp());
